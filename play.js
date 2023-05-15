388c04f2e880b32e7aafd2759b6752a29c88a551
JacquelineMG <j.gilchrist@outlook.com>
2023-05-15 14:40:06 -0700

    refactor code by separating connect function from play into client file

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require("net");
+const connect = require("./client.js");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting...");
 connect();
