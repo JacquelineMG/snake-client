e029c368591aa1b8703ae6aa9274dbd09c0882d6
JacquelineMG <j.gilchrist@outlook.com>
2023-05-16 15:52:18 -0700

    bind movement commands to keys

--- client.js
+++ client.js
@@ -6,16 +6,16 @@ const connect = function() {
     port: 50541
   });
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
+  conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: JMG");
   });
 
-  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
 
   return conn;
 };
--- input.js
+++ input.js
@@ -3,9 +3,28 @@ const handleUserInput = function(key) {
     process.exit();
   }
 
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+
 };
 
-const setupInput = function() {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
