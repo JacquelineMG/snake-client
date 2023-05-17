3f472ea42130449b3ffca0f65470a41157e1572d
JacquelineMG <j.gilchrist@outlook.com>
2023-05-16 17:24:20 -0700

    move constant variables to seperate module

--- input.js
+++ input.js
@@ -1,30 +1,29 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGE } = require("./constants");
+
+
 const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
 
-  if (key === "w") {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
 
-  if (key === "a") {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
 
-  if (key === "s") {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
 
-  if (key === "d") {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
 
-  if (key === "o") {
-    connection.write("Say: Hi");
-  }
-  
-  if (key === "p") {
-    connection.write("Say: ssss!");
+  if (MESSAGE[key]) {
+    connection.write(MESSAGE[key]);
   }
 };
 
